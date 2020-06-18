ccc7f272dbab20ce470ee860e22873b922ef8aa4
giggagit <gigg@outlook.com>
2020-06-18 22:14:12 +0700

    Fix Pyramid pattenTwo

--- number/src/main/java/com/giggagit/number/Pyramid.java
+++ number/src/main/java/com/giggagit/number/Pyramid.java
@@ -5,38 +5,37 @@ package com.giggagit.number;
  */
 public class Pyramid {
 
-    private int rowCount = 1;
+	private int rowCount = 1;
 
-    public void pattenOne(int row) {
-        for (int i = row; i > 0; i--) {
-            for (int j = i; j > 0; j--) {
-                System.out.print(" ");
-            }
+	public void pattenOne(int row) {
+		for (int i = row; i > 0; i--) {
+			for (int j = i; j > 0; j--) {
+				System.out.print(" ");
+			}
 
-            for (int j = 1; j <= rowCount; j++) {
-                System.out.print("* ");
-            }
+			for (int j = 1; j <= rowCount; j++) {
+				System.out.print("* ");
+			}
 
-            System.out.print("\n");
-            rowCount++;
-        }
-    }
+			System.out.print("\n");
+			rowCount++;
+		}
+	}
 
-    public void pattenTwo(int row) {
-        int counter = row;
-        for (int i = 1; i <= row; i++) {
-            for (int j = 1; j < rowCount; j++) {
-                System.out.print(" ");
-            }
+	public void pattenTwo(int row) {
+		for (int i = row; i > 0; i--) {
 
-            for (int j = 1; j <= counter; j++) {
-                System.out.print("* ");
-            }
+			for (int j = 1; j < rowCount; j++) {
+				System.out.print(" ");
+			}
 
-            System.out.print("\n");
-            counter--;
-            rowCount++;
-        }
-    }
+			for (int j = row; j >= rowCount; j--) {
+				System.out.print("* ");
+			}
+
+			System.out.print("\n");
+			rowCount++;
+		}
+	}
 
 }
