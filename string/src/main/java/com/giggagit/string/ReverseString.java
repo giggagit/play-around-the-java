de2ab11a8c3ced4d790a1cfbc499ac6839a416d7
giggagit <gigg@outlook.com>
2020-06-18 23:26:42 +0700

    Add reverse string with stack

--- string/src/main/java/com/giggagit/string/ReverseString.java
+++ string/src/main/java/com/giggagit/string/ReverseString.java
@@ -1,20 +1,38 @@
 package com.giggagit.string;
 
+import java.util.Stack;
+
 /**
  * ReverseString
  */
 public class ReverseString {
 
-    private String result = "";
+	private String result = "";
 
-    public String reverse(String input) {
-        String[] splitInput = input.split("");
-        
-        for (int i = splitInput.length; i > 0; i--) {
-            result += splitInput[i-1];
-        }
+	public String reverseWithArray(String input) {
+		String[] splitInput = input.split("");
 
-        return result;
-    }
+		for (int i = splitInput.length; i > 0; i--) {
+			result += splitInput[i - 1];
+		}
+
+		return result;
+	}
+
+	public String reverseWithStack(String input) {
+		char[] c = input.toCharArray();
+		int size = c.length;
+		Stack<Character> stack = new Stack<Character>();
+
+		for (int i = 0; i < size; i++) {
+			stack.push(c[i]);
+		}
+
+		for (int i = 0; i < size; i++) {
+			result += stack.pop();
+		}
+
+		return result;
+	}
 
 }
--- string/src/test/java/com/giggagit/string/ReverseStringTest.java
+++ string/src/test/java/com/giggagit/string/ReverseStringTest.java
@@ -2,6 +2,7 @@ package com.giggagit.string;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -9,12 +10,25 @@ import org.junit.jupiter.api.Test;
  */
 public class ReverseStringTest {
 
-    @Test
-    public void reverse() {
-        String input = "Hello";
-        ReverseString reverseString = new ReverseString();
-        String result = reverseString.reverse(input);
-        assertEquals("olleH", result);
-    }
+	ReverseString reverseString;
+
+	@BeforeEach
+	public void setup() {
+		reverseString = new ReverseString();
+	}
+
+	@Test
+	public void reverseWithArray() {
+		String input = "Hello";
+		String result = reverseString.reverseWithArray(input);
+		assertEquals("olleH", result);
+	}
+
+	@Test
+	public void reverseWithStack() {
+		String input = "Hello";
+		String result = reverseString.reverseWithStack(input);
+		assertEquals("olleH", result);
+	}
 
 }
